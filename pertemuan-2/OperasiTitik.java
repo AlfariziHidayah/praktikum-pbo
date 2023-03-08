ee752a887b55d4ca8fdfac0cd86248ad8818f1b3
AlfariziHidayah <100180102+AlfariziHidayah@users.noreply.github.com>
2023-03-08 16:07:39 +0700

    Add files via upload

---
+++ pertemuan-2/OperasiTitik.java
@@ -0,0 +1,27 @@
+//MTitik.java 01/03/2023
+//Nama : Alfarizi Hidayah
+//Deskripsi :kelas yang berisi operasi titik
+
+class OperasiTitik{
+	private void refleksiSumbuX(Titik titik){
+		double ordinat = titik.getOrdinat();
+		ordinat = ordinat*-1;
+		titik.setOrdinat(ordinat);
+	}
+
+	private void refleksiSumbuY(Titik titik){
+		double absis = titik.getAbsis();
+		absis = absis*-1;
+		titik.setAbsis(absis);
+	}
+
+	public Titik refleksiX(Titik titik){
+		refleksiSumbuX(titik);
+		return titik;
+	}
+
+	public Titik refleksiY(Titik titik){
+		refleksiSumbuY(titik);
+		return titik;
+	}
+}
